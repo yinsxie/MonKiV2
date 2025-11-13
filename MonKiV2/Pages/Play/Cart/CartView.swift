09793b4b786f4e3d86718764781c3803343f5e0c
aretha <aretha.wahyudi@binus.ac.id>
2025-11-13 20:35:37 +0700

    core drag & drop mechanics & game engine architecture
    - Implemented `DragManager` with environment injection for global access.
    - Added `DraggableModifier` and `DropZoneModifier` for declarative usage.
    - Implemented `PlayEngine` (ViewModel) to orchestrate `ShelfVM` and `CartVM`.
    - Created `DragOverlayView` in ZStack to handle dragging visuals.

---
+++ MonKiV2/Pages/Play/Cart/CartView.swift
@@ -0,0 +1,37 @@
+//
+//  CartView.swift
+//  MonKiV2
+//
+//  Created by Aretha Natalova Wahyudi on 13/11/25.
+//
+import SwiftUI
+
+struct CartView: View {
+    var viewModel: CartViewModel
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.gray.opacity(0.8))
+                .frame(width: 300, height: 120)
+                .shadow(radius: 5)
+            
+            HStack(spacing: -10) {
+                ForEach(viewModel.items) { item in
+                    GroceryItemView(item: item)
+                    .scaleEffect(0.8)
+                    .frame(width: 50, height: 50)
+                    .shadow(radius: 2)
+                    .transition(.scale.combined(with: .opacity))
+                }
+            }
+            .padding(.bottom, 40)
+            
+            Text("My Cart")
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding(.bottom, 10)
+        }
+        .makeDropZone(type: .cart)
+    }
+}
---
+++ MonKiV2/Pages/Play/Shelf/ShelfView.swift
@@ -0,0 +1,35 @@
+//
+//  ShelfView.swift
+//  MonKiV2
+//
+//  Created by Aretha Natalova Wahyudi on 13/11/25.
+//
+import SwiftUI
+
+struct ShelfView: View {
+    var viewModel: ShelfViewModel
+    
+    var body: some View {
+        VStack {
+            Text("Pick an Item")
+                .font(.title)
+                .padding(.top, 50)
+            
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))], spacing: 20) {
+                ForEach(viewModel.items) { item in
+                    GroceryItemView(item: item)
+                    
+                    .makeDraggable(
+                        item: DraggedItem(
+                            id: item.id,
+                            payload: .grocery(item)
+                        )
+                    )
+                }
+            }
+            .padding()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.orange.opacity(0.1))
+    }
+}
