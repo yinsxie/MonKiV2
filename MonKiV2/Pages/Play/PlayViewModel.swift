0bbc22a4c552baac38f6ced6a91be87764820039
aretha <aretha.wahyudi@binus.ac.id>
2025-11-15 17:12:45 +0700

    refactor long ahh function

--- MonKiV2/Pages/Play/PlayViewModel.swift
+++ MonKiV2/Pages/Play/PlayViewModel.swift
@@ -44,96 +44,115 @@ import SwiftUI
             guard let self = self else { return }
             
             switch draggedItem.payload {
-                
             case .grocery(let groceryItem):
-                withAnimation(.spring) {
-                    switch zone {
-                    case .cart:
-                        // source from counter
-                        DispatchQueue.main.async {
-                            if let source = draggedItem.source {
-                                switch source {
-                                case .cashierCounter:
-                                    // from counter
-                                    if let cartItem = self.cashierVM.popFromCounter(withId: draggedItem.id) {
-                                        self.cartVM.addExistingItem(cartItem)
-                                    }
-                                case .cart:
-                                    // from cart itself
-                                    break
-                                }
-                            } else {
-                                // from shelf
-                                self.cartVM.addNewItem(groceryItem)
-                            }
-                        }
-                    case .cashierLoadingCounter:
-                        print("Moved to cashier")
-                        self.handleCashierOnLoadingCounter(groceryItem: groceryItem, draggedItem: draggedItem)
-                        
-                    case .cashierRemoveItem:
-                        print("Remove from cart")
-                        if let source = draggedItem.source {
-                            switch source {
-                            case .cart:
-                                DispatchQueue.main.async {
-                                    self.cartVM.removeItem(withId: draggedItem.id)
-                                }
-                            case .cashierCounter:
-                                DispatchQueue.main.async {
-                                    self.cashierVM.removeFromCounter(withId: draggedItem.id)
-                                }
-                            }
-                        }
-                        
-                    default: break
-                    }
-                }
+                self.handleGroceryDrop(zone: zone, groceryItem: groceryItem, draggedItem: draggedItem)
             case .money(let price):
-                withAnimation(.spring) {
-                    switch zone {
-                    case .cashierPaymentCounter:
-                        print("Dropped money (\(price)) on payment counter")
-                        self.walletVM.removeItem(withId: draggedItem.id)
-                        let droppedMoney = Money(price: price)
-                        self.cashierVM.acceptMoney(droppedMoney)
-                        
-                    case .wallet:
-                        print("Dropped money (\(price)) back on wallet")
-                        
-                    default:
-                        print("Dropped money on an invalid zone (\(zone.rawValue))")
-                    }
-                }
+                self.handleMoneyDrop(zone: zone, price: price, draggedItem: draggedItem)
             }
         }
     }
 }
 
-extension PlayViewModel {
+// MARK: Drop Handlers
+private extension PlayViewModel {
+    func handleGroceryDrop(zone: DropZoneType, groceryItem: Item, draggedItem: DraggedItem) {
+        withAnimation(.spring) {
+            switch zone {
+            case .cart:
+                handleGroceryDropOnCart(groceryItem: groceryItem, draggedItem: draggedItem)
+            case .cashierLoadingCounter:
+                handleCashierOnLoadingCounter(groceryItem: groceryItem, draggedItem: draggedItem)
+            case .cashierRemoveItem:
+                handleGroceryDropOnRemoveZone(draggedItem: draggedItem)
+            default:
+                break
+            }
+        }
+    }
+    
+    func handleMoneyDrop(zone: DropZoneType, price: Int, draggedItem: DraggedItem) {
+        withAnimation(.spring) {
+            switch zone {
+            case .cashierPaymentCounter:
+                handleMoneyDropOnPaymentCounter(price: price, draggedItem: draggedItem)
+            case .wallet:
+                handleMoneyDropOnWallet(price: price, draggedItem: draggedItem)
+            default:
+                print("Dropped money on an invalid zone (\(zone.rawValue))")
+            }
+        }
+    }
+    
+    func handleGroceryDropOnCart(groceryItem: Item, draggedItem: DraggedItem) {
+        DispatchQueue.main.async {
+            if let source = draggedItem.source {
+                switch source {
+                case .cashierCounter:
+                    // from counter
+                    if let cartItem = self.cashierVM.popFromCounter(withId: draggedItem.id) {
+                        self.cartVM.addExistingItem(cartItem)
+                    }
+                case .cart:
+                    // from cart itself
+                    break
+                }
+            } else {
+                // from shelf
+                self.cartVM.addNewItem(groceryItem)
+            }
+        }
+    }
+    
+    func handleGroceryDropOnRemoveZone(draggedItem: DraggedItem) {
+        print("Remove from cart")
+        if let source = draggedItem.source {
+            switch source {
+            case .cart:
+                DispatchQueue.main.async {
+                    self.cartVM.removeItem(withId: draggedItem.id)
+                }
+            case .cashierCounter:
+                DispatchQueue.main.async {
+                    self.cashierVM.removeFromCounter(withId: draggedItem.id)
+                }
+            }
+        }
+    }
+    
+    func handleMoneyDropOnPaymentCounter(price: Int, draggedItem: DraggedItem) {
+        print("Dropped money (\(price)) on payment counter")
+        self.walletVM.removeItem(withId: draggedItem.id)
+        let droppedMoney = Money(price: price)
+        self.cashierVM.acceptMoney(droppedMoney)
+    }
+    
+    func handleMoneyDropOnWallet(price: Int, draggedItem: DraggedItem) {
+        print("Dropped money (\(price)) back on wallet")
+    }
+    
     func handleCashierOnLoadingCounter(groceryItem: Item, draggedItem: DraggedItem) {
         guard let source = draggedItem.source, source == .cart else { return }
-
+        
         DispatchQueue.main.async {
             // If counter is full
             if self.cashierVM.isLimitCounterReached() {
-
+                
                 // 1. Take the first item in the counter
                 if let firstItemInCounter = self.cashierVM.checkOutItems.first {
-
+                    
                     // 2. Remove dragged item from cart
                     if let itemToMoveToCounter = self.cartVM.popItem(withId: draggedItem.id) {
-
+                        
                         // 3. Remove the first item from counter, bring it back to cart
                         if let removedFromCounter = self.cashierVM.popFromCounter(withId: firstItemInCounter.id) {
                             self.cartVM.addExistingItem(removedFromCounter)
                         }
-
+                        
                         // 4. Add dragged item to counter
                         self.cashierVM.addToCounter(itemToMoveToCounter)
                     }
                 }
-
+                
             } else {
                 // Counter still has room
                 if let cartItem = self.cartVM.popItem(withId: draggedItem.id) {
