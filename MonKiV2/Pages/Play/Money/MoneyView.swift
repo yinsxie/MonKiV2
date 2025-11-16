2997f53493494e167e146093acc25354b73d7c08
handoyonathan <yehaccnt@gmail.com>
2025-11-16 13:16:31 +0700

    fix: adjust UI when money is being dragged

--- MonKiV2/Pages/Play/Money/MoneyView.swift
+++ MonKiV2/Pages/Play/Money/MoneyView.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 struct MoneyView: View {
     let money: Money
+    var isBeingDragged: Bool = false
     
     var body: some View {
         ZStack {
             Rectangle()
                 .fill(money.color)
-                .frame(width: 250, height: 64)
+                .frame(width: 250, height: isBeingDragged ? 128 : 64)
             
             Text("\(money.price)")
                 .font(.custom("WendyOne-Regular", size: 40))
